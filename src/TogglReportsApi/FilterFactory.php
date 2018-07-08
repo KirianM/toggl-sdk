58be1d52388a1da011b9f4c1c5ebe459f992717c
Kirian Murgadella <kirian@web4commerce.com>
2018-07-08 13:49:39 +0200

    Added Detailed and Summary filter types to the FilterFactory

--- src/TogglReportsApi/FilterFactory.php
+++ src/TogglReportsApi/FilterFactory.php
@@ -3,6 +3,8 @@
 namespace KMurgadella\TogglSdk\TogglReportsApi;
 
 use KMurgadella\TogglSdk\TogglReportsApi\Model\Filter;
+use KMurgadella\TogglSdk\TogglReportsApi\Model\FilterDetailed;
+use KMurgadella\TogglSdk\TogglReportsApi\Model\FilterSummary;
 use KMurgadella\TogglSdk\TogglReportsApi\Model\FilterWeekly;
 
 class FilterFactory
@@ -14,6 +16,14 @@ class FilterFactory
         switch ($type) {
             case 'weekly':
                 $instance = new FilterWeekly();
+            break;
+
+            case 'detailed':
+                $instance = new FilterDetailed();
+            break;
+
+            case 'summary':
+                $instance = new FilterSummary();
             break;
 
             default:
